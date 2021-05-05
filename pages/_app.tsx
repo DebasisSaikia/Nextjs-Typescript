f3ac9141af29b670c57ba8f25841351903d5eaff
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-05 22:52:06 +0530

    styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import './globals.css' //it will not work in a page or any other file..it will work only on _app.tsx file
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
 // Styled-JSX
 //if we remove golbal then it will be component scope
 
-function Heading() {
-  return (
-    <>
-      <h1>
-        Hello World
-    </h1>
-      <style jsx global>
-        {
-          `
-        h1{
-            color:red;
-        }
-       `}
-      </style>
-    </>
-  )
-}
-
+// function Heading() {
+//   return (
+//     <>
+//       <h1>
+//         Hello World
+//     </h1>
+//       <style jsx global>
+//         {
+//           `
+//         h1{
+//             color:red;
+//         }
+//        `}
+//       </style>
+//     </>
+//   )
+// }
+import styles from './Home.module.css';
 export default function Home() {
   return (
     <>
-      <Heading />
-      <h1>THis is JSX</h1>
+      {/* <Heading /> */}
+      <h1 className={styles.fw}>THis is JSX</h1>
     </>
   )
 }

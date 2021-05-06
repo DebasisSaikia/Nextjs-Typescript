9f5e2627e8d84c937b4ea2a4901bedb29524e966
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-06 14:05:18 +0530

    dynamic routing

---
+++ pages/fruit/[name]/index.tsx
@@ -0,0 +1,3 @@
+export default function () {
+    return <h1>Index Page of Fruid</h1>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,10 @@
-// Styled-JSX
-//if we remove golbal then it will be component scope
-
-// function Heading() {
-//   return (
-//     <>
-//       <h1>
-//         Hello World
-//     </h1>
-//       <style jsx global>
-//         {
-//           `
-//         h1{
-//             color:red;
-//         }
-//        `}
-//       </style>
-//     </>
-//   )
-// }
-import styles from './Home.module.css';
-export default function Home() {
-  return (
-    <>
-      {/* <Heading /> */}
-      <h1 className={styles.fw}>THis is JSX</h1>
-    </>
-  )
+import styles from './style.module.scss';
+function Home() {
+    return (
+        <div>
+            <h1 className={styles.red}>This is page 1</h1>
+        </div>
+    )
 }
+
+export default Home

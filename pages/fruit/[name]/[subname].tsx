85ab8c2f7cf285013f2eee145debd7df05320694
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-07 14:21:38 +0530

    use-router

--- pages/fruit/[name]/[subname].tsx
+++ pages/fruit/[name]/[subname].tsx
@@ -1,3 +1,11 @@
+import { useRouter } from 'next/router'
 export default function () {
-    return <h1>Hello Dynamic routing</h1>
+    const router = useRouter()
+    // replace will push the given url and cannot access back th1 page but push  allow too access that page
+    return (
+        <>
+            <h1>Hello Dynamic routing</h1>
+            <button onClick={() => router.replace('/')}>Home</button>
+        </>
+    )
 }
